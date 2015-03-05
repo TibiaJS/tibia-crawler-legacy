7a2653508d0cbf4c80f21c717a8bf66e6d0906af
Renato Ribeiro <renatoribroman@gmail.com>
2015-03-05 02:02:08 -0300

    world fetcher addictions

--- src/fetcher/world.js
+++ src/fetcher/world.js
@@ -19,16 +19,24 @@ function World($){
   getWrapper('World Information')
   .find('tr')
   .each(function(){
-    var index = $(this).find('td:nth-child(1)').text()
+    var key = $(this).find('td:nth-child(1)').text()
       .replace(':', '')
       .replace(/\s/g, '_')
       .toLowerCase();
     var value = $(this).find('td:nth-child(2)').text().trim();
-    if(index == 'world_quest_titles'){
+
+    if(key == 'players_online'){
+      value = parseInt(value);
+    }
+
+    if(key == 'world_quest_titles'){
       value = value.split(', ');
     }
-    self.world[index] = value;
+
+    self.world[key] = value;
   });
+  this.world['online'] = this.world['status'] == 'Online';
+
 
   getWrapper('Players Online')
   .find('tr:nth-child(n+2)')
