b794f6908070e81a16e79137c416c565fda4b370
Ranieri Althoff <ranisalt@gmail.com>
2015-03-10 11:15:11 -0300

    Parse spell data

--- src/fetcher/spell.js
+++ src/fetcher/spell.js
@@ -19,8 +19,34 @@ function Spell($) {
         return $('.BoxContent table');
     };
 
-    $(getWrapper()[1]).find('tr:nth-child(n+2)').each(function(index, item) {
+    $(getWrapper()[1]).find('tr:nth-child(n+2)').each(function() {
+        var key = util.camelCase($(this).find('td:nth-child(1)').text());
+        var value = $(this).find('td:nth-child(2)').text().trim();
 
+        switch (key) {
+            case 'vocation':
+            case 'city':
+                value = value.split(', ');
+                break;
+            case 'group':
+            case 'type':
+            case 'damageType':
+                value = value.toLowerCase();
+                break;
+            case 'cooldown':
+                var match = value.match(/(\d+)/g);
+                value = match[0];
+                self.spell.groupCooldown = match[1];
+                break;
+            case 'expLvl':
+                key = 'minLevel';
+                break;
+            case 'premium':
+                value = value === 'yes';
+                break;
+        }
+
+        self.spell[key] = value;
     });
 
     return this;
