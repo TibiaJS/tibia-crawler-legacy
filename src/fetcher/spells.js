00994a9a950ce0956a0ca0400ff990b5ae75e135
Ranieri Althoff <ranisalt@gmail.com>
2015-03-10 01:25:05 -0300

    Get spell list successfully

--- src/fetcher/spells.js
+++ src/fetcher/spells.js
@@ -4,6 +4,26 @@ function Spells($) {
 
     this.spells = [];
     var self = this;
+
+    var getWrapper = function() {
+        return $('.BoxContent table').first();
+    };
+
+    getWrapper().find('tr').each(function(index, item) {
+        item = $(item).find('td');
+
+        self.spells.push({
+            name: $(item[0]).text(),
+            group: $(item[1]).text(),
+            type: $(item[2]).text(),
+            minLevel: parseInt($(item[3]).text()),
+            mana: parseInt($(item[4]).text()),
+            price: $(item[5]).text() === 'free' ? 0 : parseInt($(item[5]).text()),
+            premium: $(item[6]).text() === 'yes'
+        });
+    });
+
+    this.spells.shift();
 }
 
 module.exports = Spells;
