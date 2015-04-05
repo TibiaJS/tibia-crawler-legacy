0b029489eda1c50c187bbf6eb86c42e69360e99d
Renato Ribeiro <renatoribroman@gmail.com>
2015-04-05 01:49:07 -0300

    throw request errors

--- src/index.js
+++ src/index.js
@@ -12,76 +12,98 @@ var KillStatistics = require('./fetchers/killstatistics');
 var TibiaCrawler = {
 
     character: function(name, cb) {
-        name = name.replace(' ', '+').replace('%20', '+');
-        var path = 'community/?subtopic=characters&name=' + name;
-        return api.request('get', path, {}, function(err, res, body) {
-            var $ = cheerio.load(body);
-            if ($('b:contains("Could not find character")').closest('table').length === 1) {
-                cb();
-            } else {
-                var player = new Character($);
-                cb(player);
-            }
-        });
+      name = name.replace(' ', '+').replace('%20', '+');
+      var path = 'community/?subtopic=characters&name=' + name;
+      return api.request('get', path, {}, function(err, res, body) {
+        if(!err){
+          var $ = cheerio.load(body);
+          if ($('b:contains("Could not find character")').closest('table').length === 1) {
+            cb(null);
+          } else {
+            var player = new Character($);
+            cb(player);
+          }
+        }
+      });
     },
 
     spells: function(cb) {
-        var path = 'library/?subtopic=spells';
-        return api.request('get', path, {}, function(err, res, body) {
-            cb(new Spells(cheerio.load(body)));
-        });
+      var path = 'library/?subtopic=spells';
+      return api.request('get', path, {}, function(err, res, body) {
+        if(!err){
+          cb(new Spells(cheerio.load(body)));
+        } else {
+          throw new Error('Request error');
+        }
+      });
     },
 
     highscores: function(world, category, page, cb) {
-        var categories = ['experience', 'magic', 'shielding', 'distance', 'sword', 'club', 'axe', 'fist', 'fishing', 'achievements', 'loyalty'];
+      var categories = ['experience', 'magic', 'shielding', 'distance', 'sword', 'club', 'axe', 'fist', 'fishing', 'achievements', 'loyalty'];
 
-        category = category || 'experience';
-        if(categories.indexOf(category) === -1) {
-          throw new Error('Unknown ' + category + ' category. Avaliables: ' + categories.join(', ') + '.');
+      category = category || 'experience';
+      if(categories.indexOf(category) === -1) {
+        throw new Error('Unknown ' + category + ' category. Avaliables: ' + categories.join(', ') + '.');
+      }
+
+      //TODO: Cache this
+      this.world(world, function(exists) {
+
+        if(exists) {
+          page = page || 0;
+
+          var path = 'community/?subtopic=highscores&world=' + world + '&list=' + category + '&page=' + parseInt(page);
+          return api.request('get', path, {}, function(err, res, body) {
+            if(!err){
+              var $ = cheerio.load(body);
+              cb(new Highscores($, category));
+            } else {
+              throw new Error('Request error');
+            }
+          });
+        } else {
+          throw new Error('Unknown world name ' + world + '.');
         }
 
-        //TODO: Cache this
-        this.world(world, function(exists) {
-
-          if(exists) {
-            page = page || 0;
-
-            var path = 'community/?subtopic=highscores&world=' + world + '&list=' + category + '&page=' + parseInt(page);
-            return api.request('get', path, {}, function(err, res, body) {
-                var $ = cheerio.load(body);
-                cb(new Highscores($, category));
-            });
-          } else {
-            throw new Error('Unknown world name ' + world + '.');
-          }
-
-        });
+      });
     },
 
     world: function(name, cb) {
-        var path = 'community/?subtopic=worlds&world=' + name;
-        return api.request('get', path, {}, function(err, res, body) {
-            var $ = cheerio.load(body);
-            if ($('.red:contains("World with this name doesn\'t exist!")').length === 1) {
-                cb();
-            } else {
-                cb(new World($));
-            }
-        });
+      var path = 'community/?subtopic=worlds&world=' + name;
+      return api.request('get', path, {}, function(err, res, body) {
+        if(!err){
+          var $ = cheerio.load(body);
+          if ($('.red:contains("World with this name doesn\'t exist!")').length === 1) {
+              cb();
+          } else {
+              cb(new World($));
+          }
+        } else {
+          throw new Error('Request error');
+        }
+      });
     },
 
     worlds: function(cb) {
-        var path = 'community/?subtopic=worlds';
-        return api.request('get', path, {}, function(err, res, body) {
-            cb(new Worlds(cheerio.load(body)));
-        });
+      var path = 'community/?subtopic=worlds';
+      return api.request('get', path, {}, function(err, res, body) {
+        if(!err){
+          cb(new Worlds(cheerio.load(body)));
+        } else {
+          throw new Error('Request error');
+        }
+      });
     },
 
     killStatistics: function(world, cb){
-        var path = 'community/?subtopic=killstatistics&world=' + world;
-        return api.request('get', path, {}, function(err, res, body) {
-            cb(new KillStatistics(cheerio.load(body)));
-        });
+      var path = 'community/?subtopic=killstatistics&world=' + world;
+      return api.request('get', path, {}, function(err, res, body) {
+        if(!err){
+          cb(new KillStatistics(cheerio.load(body)));
+        } else {
+          throw new Error('Request error');
+        }
+      });
     }
 
 };
